59c7cf95ca98ceaac3a98b3820e733eb1fe51fae
hydrorxodin <hydrorxodin@Xubuntu.myguest.virtualbox.org>
2024-09-30 14:19:12 +0300

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const baseConfig = require("./base.config.js");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  ...baseConfig,
+};
